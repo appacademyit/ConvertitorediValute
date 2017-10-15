85e3910433cf8845da185616d135bb0cbfdbb047
cosimopalma <cosimopalma@gmail.com>
2017-10-15 20:18:45 +0200

    Initial commit

---
+++ app/src/main/java/it/appacademy/convertitoredivalute/MainActivity.java
@@ -0,0 +1,45 @@
+package it.appacademy.convertitoredivalute;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+// TODO 1: Dichiarare le View con le quali si lavorerà
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // TODO 2: Collegare le Views
+
+
+
+    }
+
+
+
+    public void convertiValuta(View view) {
+
+        // TODO 3. Log Button Click
+
+
+        // TODO 4. Leggere euro e Log
+
+
+        // TODO 5. Trasformare gli euro letti da String a Double
+
+
+        // TODO 6. Convertire Euro in dollari
+        // dollari = euro * 1.18213;
+
+
+
+        // TODO 7. Scrivere il valore espresso in Dollari nella TextView
+
+
+    }
+}
